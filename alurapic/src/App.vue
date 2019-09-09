c5b72b2daa313fc500ce6b5244ecd32b50ab6c32
Guilherme Ribeiro <guifrribeiro@gmail.com>
2019-09-09 19:23:17 -0300

    Initial Commit

---
+++ alurapic/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+    <ul>
+      <li v-for="foto in fotos">
+        <img :src="foto.url" :alt="foto.titulo">
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      titulo: 'AluraPic',
+      fotos: [
+        {
+          url: 'https://www.petz.com.br/blog/wp-content/uploads/2019/04/como-saber-se-o-cachorro-esta-com-febre-doencas.jpg',
+          titulo: 'cachorro'
+        },
+        {
+          url: 'https://www.petz.com.br/blog/wp-content/uploads/2019/04/como-saber-se-o-cachorro-esta-com-febre-doencas.jpg',
+          titulo: 'cachorro'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
