0e42905c97a121ba247c0b407ae2d9491d20d637
Guilherme Ribeiro <guifrribeiro@gmail.com>
2019-09-10 00:12:45 -0300

    Api and beginning of stylization

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
-    <h1>{{ titulo }}</h1>
-    <ul>
-      <li v-for="foto in fotos">
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto in fotos">
         <img :src="foto.url" :alt="foto.titulo">
       </li>
     </ul>
@@ -14,20 +14,33 @@ export default {
   data() {
     return {
       titulo: 'AluraPic',
-      fotos: [
-        {
-          url: 'https://www.petz.com.br/blog/wp-content/uploads/2019/04/como-saber-se-o-cachorro-esta-com-febre-doencas.jpg',
-          titulo: 'cachorro'
-        },
-        {
-          url: 'https://www.petz.com.br/blog/wp-content/uploads/2019/04/como-saber-se-o-cachorro-esta-com-febre-doencas.jpg',
-          titulo: 'cachorro'
-        }
-      ]
+      fotos: []
     }
+  },
+  created() {
+    this.$http.get('http://localhost:3000/v1/fotos')
+      .then(res => res.json())
+      .then(fotos => this.fotos = fotos, err => console.log(err))
   }
 }
 </script>
 
 <style>
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    width: 96%;
+    margin: 0 auto;
+  }
+
+  .centralizado {
+    text-align: center;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
 </style>
